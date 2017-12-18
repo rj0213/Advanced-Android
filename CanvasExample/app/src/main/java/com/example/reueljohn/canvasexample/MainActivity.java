3abc5dfaf774db6379231da373df07b4dd155350
Reuel John <reueljohn@MAC077.local>
2017-12-18 09:29:16 +0800

    11.1B

---
+++ CanvasExample/app/src/main/java/com/example/reueljohn/canvasexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.reueljohn.canvasexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MyCanvasView myCanvasView;
+
+        myCanvasView = new MyCanvasView(this);
+
+        myCanvasView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(myCanvasView);
+    }
+}
